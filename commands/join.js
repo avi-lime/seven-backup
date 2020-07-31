a1330b2f15ec404202174ee43674695d36f0c85c
angi-mint <68991185+angi-mint@users.noreply.github.com>
2020-07-31 14:16:54 +0200

    added some missing return statements in join handler

--- commands/join.js
+++ commands/join.js
@@ -2,20 +2,25 @@ module.exports = {
     name: 'join',
 	description: 'TODO',
 	execute(message, args) {
-        if (!args[0])
-            { return message.channel.send('mention a team to join, currently available teams are `angi` and `avi`!'); }
+        if (!args[0]) {
+            message.channel.send('mention a team to join, currently available teams are `angi` and `avi`!');
+            return;
+        }
             
         const team = args[0].toLowerCase();
         if (team === 'avi') {
             const role = message.guild.roles.cache.find(role => role.name === "アビ 分隊 — avi's squad");
             message.member.roles.add(role).then(message.channel.send('you joined team avi'));
+            return;
         }
         if (team === 'angi') {
             const role = message.guild.roles.cache.find(role => role.name === "天使 分隊 — angi's squad");
             message.member.roles.add(role).then(message.channel.send('you joined team angi'));
+            return;
         }
         if (team !== 'avi' || team !== 'angi') {
             message.channel.send('mention a valid team to join, either `angi` or `avi`');
+            return;
         }
     }
 };
