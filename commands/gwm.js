ec82180ad1a62ef4a149756ba6c03f34b7a73dec
Avi <67220821+avi-lime@users.noreply.github.com>
2020-08-03 00:47:08 +0530

    Update gwm.js

--- commands/gwm.js
+++ commands/gwm.js
@@ -7,12 +7,12 @@ module.exports = {
         const args = msg.content.slice(prefix.length).trim().split(/ +/);
         const user = msg.mentions.members.first();
 
-
         if (!msg.member.roles.cache.some(r => ['✈ 守護天使 — angi', '❦ 管理人 — admin', '.•° head mod  °•.'].includes(r.name))) return;
         if (!user) {
             return msg.channel.send('mention someone to appoint as giveaway manager');
         }
         if (args[0] === 'fix') {
+
             const ran_reply = [`successfully added missing roles for **${user.displayName}**`, `all roles for **${user.displayName}** have been fixed`, `successfully fixed **${user.displayName}**'s' roles`, `all roles have been fixed for **${user.displayname}**`, `successfully fixed roles for **${user.displayName}**`];
             const rr = ran_reply[Math.floor(Math.random() * ran_reply.length)];
             msg.delete().then
@@ -24,12 +24,11 @@ module.exports = {
             });
             return;
         }
-        const proBy = msg.author.tag;
         msg.delete().then
         user.roles.add(['735437137356521583', '688386729807577284', '690566791407206431']).then
         msg.channel.send(`<a:sevenloading:739558030420475904> adding roles to **${user.displayName}**...`).then(sentMessage => {
             setTimeout(function () {
-                sentMessage.edit(`:tada: | Congratulations ${user}! ` + proBy + ` has promoted you to **giveaway manager**.`)
+                sentMessage.edit(`:tada: | Congratulations ${user}! ${msg.author.tag} has promoted you to **giveaway manager**.`)
             }, 1500)
         });
     }
