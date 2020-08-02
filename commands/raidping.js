ed309eaf195026f181c5491a5e59ad68db458f23
Avi <67220821+avi-lime@users.noreply.github.com>
2020-08-02 20:41:49 +0530

    Update raidping.js

--- commands/raidping.js
+++ commands/raidping.js
@@ -2,14 +2,13 @@ const prefix = '-';
 module.exports = {
     name: 'raidping',
     description: 'pings the raid role',
-    execute(message) {
-        var raidchannels = 688108643966779420;
+    execute(msg) {
+        const raidchannels = ['735089552326393957', '735089631871238155', '735089717703606345', '735089719712677919', '735089800574664715', '735089802944446474', '735791071115870271', '735802535272775710', '735802613563523142', '735802656588562477'];
 
-        const args = message.content.slice(prefix.length).trim().split(/ +/);
+        const args = msg.content.slice(prefix.length).trim().split(/ +/);
         const commandName = args.shift().toLowerCase();
 
-        if (!message.member.roles.cache.has(735091954047647745) /* || message.channel.id != raidchannels */)
-            return;
-        message.channel.send('<@&688438198116024345>' + message.content.replace(prefix + commandName, " "));
+        if (!msg.member.roles.cache.has('735091954047647745') || !raidchannels.includes(msg.channel.id)) return;
+        msg.channel.send('<@&688438198116024345>' + msg.content.replace(prefix + commandName, " "));
     }
 }
