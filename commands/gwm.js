dee5cc260511198a14e6f86d39a9677c0c41caa5
Avi <67220821+avi-lime@users.noreply.github.com>
2020-08-03 01:02:42 +0530

    Update gwm.js

--- commands/gwm.js
+++ commands/gwm.js
@@ -6,12 +6,12 @@ module.exports = {
     execute(msg) {
         const args = msg.content.slice(prefix.length).trim().split(/ +/);
         const user = msg.mentions.members.first();
-
+        const sub = args[0];
         if (!msg.member.roles.cache.some(r => ['✈ 守護天使 — angi', '❦ 管理人 — admin', '.•° head mod  °•.'].includes(r.name))) return;
         if (!user) {
             return msg.channel.send('mention someone to appoint as giveaway manager');
         }
-        if (args[0] === 'fix') {
+        if (sub === 'fix') {
 
             const ran_reply = [`successfully added missing roles for **${user.displayName}**`, `all roles for **${user.displayName}** have been fixed`, `successfully fixed **${user.displayName}**'s' roles`, `all roles have been fixed for **${user.displayname}**`, `successfully fixed roles for **${user.displayName}**`];
             const rr = ran_reply[Math.floor(Math.random() * ran_reply.length)];
@@ -23,7 +23,7 @@ module.exports = {
                 }, 1500)
             });
             return;
-        } else {
+        } else if (sub !== 'fix') {
             msg.delete().then
             user.roles.add(['735437137356521583', '688386729807577284', '690566791407206431']).then
             msg.channel.send(`<a:sevenloading:739558030420475904> adding roles to **${user.displayName}**...`).then(sentMessage => {
