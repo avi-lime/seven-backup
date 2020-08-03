600e724a5163d95ba866f7300e2ec238daa55c3b
Avi <67220821+avi-lime@users.noreply.github.com>
2020-08-03 17:59:21 +0530

    cooldown in raidping

--- commands/raidping.js
+++ commands/raidping.js
@@ -1,14 +1,25 @@
 const prefix = '-';
+const lastChan = new Map();
+const Duration = require('humanize-duration');
 module.exports = {
     name: 'raidping',
     description: 'pings the raid role',
     execute(msg) {
+        const cooldown = lastChan.get(msg.channel.id);
         const raidchannels = ['735089552326393957', '735089631871238155', '735089717703606345', '735089719712677919', '735089800574664715', '735089802944446474', '735791071115870271', '735802535272775710', '735802613563523142', '735802656588562477'];
 
         const args = msg.content.slice(prefix.length).trim().split(/ +/);
         const commandName = args.shift().toLowerCase();
 
         if (!msg.member.roles.cache.has('735091954047647745') || !raidchannels.includes(msg.channel.id)) return;
-        msg.channel.send('<@&688438198116024345>' + msg.content.replace(prefix + commandName, " "));
+        if (cooldown) {
+            const remaining = Duration(cooldown - Date.now(), { units: ['m', 's'], round: true });
+            return msg.channel.send(`This command was just used in this channel! \nYou need to wait **${remaining}** before using it again.`);
+        }
+        else {
+            msg.channel.send('<@&688438198116024345>' + msg.content.replace(prefix + commandName, " "));
+            lastChan.set(msg.channel.id, Date.now() + 1000 * 60 * 5);
+            setTimeout(() => { lastChan.delete(msg.channel.id), 1000 * 60 * 5 });
+        }
     }
 }
