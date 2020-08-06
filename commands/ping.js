03c4c078f5b0ee34dd923ac74ccaeb12baac50e7
Avi <67220821+avi-lime@users.noreply.github.com>
2020-08-06 21:53:16 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,10 +2,10 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	execute(msg) {
-		const time = msg.createdTimestamp;
+		const pong = Date.now() - msg.createdTimestamp;
 		msg.channel.send(`<a:sevenloading:739558030420475904> Pinging...`).then(m => {
 			setTimeout(function () {
-				m.edit(`Pong! \`${Date.now() - time}ms\``);
+				m.edit(`Pong! \`${pong}ms\``);
 			}, 1500);
 		})
 	}
