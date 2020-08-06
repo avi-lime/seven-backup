c9f5c8e62d11e251d190be9c800c630722189fd6
Avi <67220821+avi-lime@users.noreply.github.com>
2020-08-06 21:37:12 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,8 +4,8 @@ module.exports = {
 	execute(msg) {
 		msg.channel.send(`<a:sevenloading:739558030420475904> Pinging...`).then(m => {
 			setTimeout(function () {
-				m.edit(`Pong! ${Date.now() - msg.createdTimestamp}ms`);
-			})
+				m.edit(`Pong! \`${Date.now() - msg.createdTimestamp}ms\``);
+			}, 1500);
 		})
 	}
 };
