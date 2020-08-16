da47e772837ee5849076c91887b484b3c81eb121
Avi <67220821+avi-lime@users.noreply.github.com>
2020-08-16 13:23:03 +0530

    msg -> message

--- commands/claim.js
+++ commands/claim.js
@@ -3,17 +3,17 @@ const Discord = require('discord.js');
 module.exports = {
     name: 'claim',
     description: 'claims a channel by the user',
-    execute(msg) {
-        const args = msg.content.slice(prefix.length).trim().split(/ +/);
-        const user = msg.mentions.members.first();
+    execute(message) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+        const user = message.mentions.members.first();
         const sub = args[1];
         if (sub === 'fix') {
-            if (!msg.member.roles.cache.some(r => ['✈ 守護天使 — angi', '❦ 管理人 — admin', '.•° head mod  °•.'].includes(r.name))) return;
+            if (!message.member.roles.cache.some(r => ['✈ 守護天使 — angi', '❦ 管理人 — admin', '.•° head mod  °•.'].includes(r.name))) return;
 
             if (user.roles.cache.has('735865064996732948'))
-                return msg.channel.send(`${user} already has the required role!`);
+                return message.channel.send(`${user} already has the required role!`);
             else {
-                msg.channel.send(`<a:sevenloading:739558030420475904> fixing roles for ${user.displayName}...`).then(sentMessage => {
+                message.channel.send(`<a:sevenloading:739558030420475904> fixing roles for ${user.displayName}...`).then(sentMessage => {
                     setTimeout(function () {
                         sentMessage.edit(`<a:sevensuccess:740956620678234222> fixed roles for ${user}!`)
                     }, 1500);
@@ -22,12 +22,12 @@ module.exports = {
                 return;
             }
         } else if (sub === 'ban') {
-            if (!msg.member.roles.cache.some(r => ['✈ 守護天使 — angi', '❦ 管理人 — admin', '.•° head mod  °•.'].includes(r.name))) return;
+            if (!message.member.roles.cache.some(r => ['✈ 守護天使 — angi', '❦ 管理人 — admin', '.•° head mod  °•.'].includes(r.name))) return;
 
             if (!user.roles.cache.has('735865064996732948'))
-                return msg.channel.send(`${user} is already banned!`);
+                return message.channel.send(`${user} is already banned!`);
             else {
-                msg.channel.send(`<a:sevenloading:739558030420475904> banning ${user.displayName} from claiming channels...`).then(sentMessage => {
+                message.channel.send(`<a:sevenloading:739558030420475904> banning ${user.displayName} from claiming channels...`).then(sentMessage => {
                     setTimeout(function () {
                         sentMessage.edit(`<a:sevensuccess:740956620678234222> ${user} has been banned from claiming channels by ${msg.author.tag}!`)
                     }, 1500);
@@ -37,40 +37,40 @@ module.exports = {
             }
         } else if (sub !== 'ban' || sub !== 'fix') {
             const raidchannels = ['735089552326393957', '735089631871238155', '735089717703606345', '735089719712677919', '735089800574664715', '735089802944446474', '735791071115870271', '735802535272775710', '735802613563523142', '735802656588562477', '688108643966779420'];
-            if (!raidchannels.includes(msg.channel.id)) return;
-            if (!msg.member.roles.cache.has('735865064996732948')) {
+            if (!raidchannels.includes(message.channel.id)) return;
+            if (!message.member.roles.cache.has('735865064996732948')) {
                 const banned = new Discord.MessageEmbed()
                     .setTitle(`Can't claim!`)
-                    .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }))
+                    .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
                     .setColor("#9b4b45")
                     .setDescription(`you've either already claimed a channel or you've been banned from claiming channels!`)
                     .setFooter(`contact staff for more info`)
-                return msg.channel.send(banned);
+                return message.channel.send(banned);
             } else {
-                let channel = msg.guild.channels.cache.get('735884448280608798');
-                const claimreply = [`${msg.author} you successfully claimed this adventure channel!\n  - have fun playing and don't forget to \`${prefix}unclaim\` `, `This adventure channel has been claimed by ${msg.author} \n  - don't forget to \`${prefix}unclaim\` once you're done playing`, `${msg.author} I've successfully claimed this channel for you! \n  - use \`${prefix}unclaim\`, when you finished playing`];
+                let channel = message.guild.channels.cache.get('735884448280608798');
+                const claimreply = [`${message.author} you successfully claimed this adventure channel!\n  - have fun playing and don't forget to \`${prefix}unclaim\` `, `This adventure channel has been claimed by ${msg.author} \n  - don't forget to \`${prefix}unclaim\` once you're done playing`, `${msg.author} I've successfully claimed this channel for you! \n  - use \`${prefix}unclaim\`, when you finished playing`];
                 const claimchan = new Discord.MessageEmbed()
                     .setTitle('claimed')
-                    .setDescription(`${msg.channel} has been claimed by ${msg.author}`)
+                    .setDescription(`${message.channel} has been claimed by ${message.author}`)
                     .setFooter('enjoy~')
                     .setColor('#D2CB97')
                     .setTimestamp()
-                    .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }));
+                    .setThumbnail(message.author.displayAvatarURL({ dynamic: true }));
                 const claimed = new Discord.MessageEmbed()
                     .setTitle('Channel Claimed!')
                     .setDescription(claimreply[Math.floor(Math.random() * claimreply.length)])
                     .setColor('#D2CB97')
-                    .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }))
+                    .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
                     .setTimestamp()
                     .setFooter('enjoy~');
-                msg.delete().then
-                msg.channel.updateOverwrite(msg.channel.guild.roles.everyone, { SEND_MESSAGES: false }).then
-                msg.channel.updateOverwrite(msg.author.id, { SEND_MESSAGES: true }).then
-                msg.member.roles.remove('735865064996732948').then
-                msg.channel.send(`<a:sevenloading:739558030420475904> claiming channel ...`).then(sentMessage => {
+                message.delete().then
+                message.channel.updateOverwrite(message.channel.guild.roles.everyone, { SEND_MESSAGES: false }).then
+                message.channel.updateOverwrite(message.author.id, { SEND_MESSAGES: true }).then
+                message.member.roles.remove('735865064996732948').then
+                message.channel.send(`<a:sevenloading:739558030420475904> claiming channel ...`).then(sentMessage => {
                     setTimeout(function () {
                         sentMessage.delete().then
-                        msg.channel.send(claimed)
+                        message.channel.send(claimed)
                     }, 1500);
                 }).then
                 channel.send(claimchan);
--- commands/guess.js
+++ commands/guess.js
@@ -5,9 +5,9 @@ module.exports = {
     name: 'guess',
     aliases: ['number'],
     description: 'guess the number and win the prize',
-    execute(msg) {
-        if (!msg.member.roles.cache.some(r => ['✈ 守護天使 — angi', '❦ 管理人 — admin', '.•° head mod  °•.', '°•. mod .•°', '催し主事 — event manager'].includes(r.name))) return;
-        const args = msg.content.slice(prefix.length).trim().split(/ +/);
+    execute(message) {
+        if (!message.member.roles.cache.some(r => ['✈ 守護天使 — angi', '❦ 管理人 — admin', '.•° head mod  °•.', '°•. mod .•°', '催し主事 — event manager'].includes(r.name))) return;
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
         const commandName = args.shift().toLowerCase();
         const sub = args[0];
 
@@ -15,70 +15,70 @@ module.exports = {
             const range = args[1];
             const time = args[2];
             const endtime = time * 60 * 1000;
-            const prize = msg.content.replace(prefix + commandName + ' ' + sub + ' ' + range + ' ' + time, '');
-            if (isNaN(range)) return msg.channel.send('set the maximum number');
-            if (isNaN(time)) return msg.channel.send('set a time for the game');
-            if (!args[3]) return msg.channel.send(`set a prize`);
+            const prize = message.content.replace(prefix + commandName + ' ' + sub + ' ' + range + ' ' + time, '');
+            if (isNaN(range)) return message.channel.send('set the maximum number');
+            if (isNaN(time)) return message.channel.send('set a time for the game');
+            if (!args[3]) return message.channel.send(`set a prize`);
             const answer = Math.floor(Math.random() * range);
             var ans = new RegExp("^" + answer + "$", "g")
             const filter = a => a.content.match(ans);
             const startmsg = new Discord.MessageEmbed()
                 .setTitle(`GAME STARTED`)
                 .setDescription(`Guess the number between **0 to ${range}** in **${time} minutes**\n  ➵ First to guess the number wins**${prize}**`)
-                .setColor(msg.member.displayHexColor)
+                .setColor(message.member.displayHexColor)
                 .setTimestamp()
                 .setFooter(`Good Luck`)
-                .setThumbnail(msg.guild.iconURL({ dynamic: true }));
+                .setThumbnail(message.guild.iconURL({ dynamic: true }));
             const foradmin = new Discord.MessageEmbed()
                 .setTitle("ANSWER")
-                .setColor(msg.member.displayHexColor)
+                .setColor(message.member.displayHexColor)
                 .setDescription(`The number is **${answer}**!`)
-                .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }))
+                .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
                 .setFooter('Good Luck');
 
-            msg.delete().then
-            msg.guild.channels.cache.get('688108643966779420').send(foradmin).then
-            msg.channel.updateOverwrite(msg.channel.guild.roles.everyone, { SEND_MESSAGES: null }).then
-            msg.channel.send(startmsg).then(() => {
-                msg.channel.awaitMessages(filter, { max: 1, time: endtime, errors: ['time'] })
+            message.delete().then
+            message.guild.channels.cache.get('688108643966779420').send(foradmin).then
+            message.channel.updateOverwrite(message.channel.guild.roles.everyone, { SEND_MESSAGES: null }).then
+            message.channel.send(startmsg).then(() => {
+                message.channel.awaitMessages(filter, { max: 1, time: endtime, errors: ['time'] })
                     .then(collected => {
                         const winner = new Discord.MessageEmbed()
                             .setTitle(':tada: CONGRATULATIONS :tada:')
                             .setDescription(`${collected.first().author} guessed the correct number!\n  ➵ The number was **${answer}**\n  ➵ You won**${prize}**`)
                             .setFooter(`thanks for playing`)
-                            .setColor(collected.first().member.displayHexColor)
+                            .setColor(collected.first().member.displayHexColor)message
                             .setThumbnail(collected.first().author.displayAvatarURL({ dynamic: true }));
-                        msg.channel.send(winner).then
-                        msg.channel.updateOverwrite(msg.channel.guild.roles.everyone, { SEND_MESSAGES: false });
-                    })
-                    .catch(collected => {
-                        const timeup = new Discord.MessageEmbed()
-                            .setColor('RED')
-                            .setTitle("TIME UP")
-                            .setDescription(`oop, looks like no one could guess the number!\n  ➵ The number was **${answer}**`)
-                            .setThumbnail(msg.guild.iconURL({ dynamic: true }))
-                            .setFooter('better luck next time');
-                        msg.channel.send(timeup).then
-                        msg.channel.updateOverwrite(msg.channel.guild.roles.everyone, { SEND_MESSAGES: false });
-                    })
-            });
-        } else if (sub === 'rules') {
-            const rules = new Discord.MessageEmbed()
-                .setColor('#aab5ee')
-                .setTitle('✦ RULES ✦')
-                .setDescription(`➵ You have to guess the number in the **given time period** and within the **given range** of numbers.\n➵ You'll get the **mentioned** prize if you guess the correct number.\n➵ The channel will be unlocked once the game starts and will be locked again if the time runs out or someone guesses the number\n➵ You can spam as much as you want and get **unlimited attempts** to the guess the number. \n`)
-                .setFooter('⇀ contact staff for other queries')
-                .setThumbnail(msg.guild.iconURL({ dynamic: true }));
-            msg.delete().then
-            msg.channel.send(rules);
-        } else if (sub === 'help') {
-            const help = new Discord.MessageEmbed()
-                .setColor('#aab5ee')
-                .setTitle('✧ GUESS THE NUMBER ✧')
-                .setDescription(`➵ Command usage:\n> \`guess start <range> <time> <prize>\`\n   - \`<range>\`  to fix the range from 0, for the random number\n   - \`<time>\`  sets the time limit for the game, **in minutes**\n   - \`<prize>\`  to set the prize for the winner\n\n ➵ The command won't work if any of the fields are missing.\n ➵ The game will end once either someone guesses the number or the time runs out.\n ➵ The channel will be locked once the game end, you're immune to this if you're starting the game.`)
-                .setFooter(`⇀ contact 𝐨𝐜𝐭𝐨𝐛𝐞𝐫#7777 or 𝐯𝐢𝐯𝐢#3952, if you have any other questions.`)
-                .setThumbnail(msg.guild.iconURL({ dynamic: true }));
-            msg.channel.send(help);
-        }
+                message.channel.send(winner).then
+                message.channel.updateOverwrite(message.channel.guild.roles.everyone, { SEND_MESSAGES: false });
+            })
+                .catch(collected => {
+                    const timeup = new Discord.MessageEmbed()
+                        .setColor('RED')
+                        .setTitle("TIME UP")
+                        .setDescription(`oop, looks like no one could guess the number!\n  ➵ The number was **${answer}**`)
+                        .setThumbnail(message.guild.iconURL({ dynamic: true }))
+                        .setFooter('better luck next time');
+                    message.channel.send(timeup).then
+                    message.channel.updateOverwrite(message.channel.guild.roles.everyone, { SEND_MESSAGES: false });
+                })
+        });
+    } else if(sub === 'rules') {
+    const rules = new Discord.MessageEmbed()
+        .setColor('#aab5ee')
+        .setTitle('✦ RULES ✦')
+        .setDescription(`➵ You have to guess the number in the **given time period** and within the **given range** of numbers.\n➵ You'll get the **mentioned** prize if you guess the correct number.\n➵ The channel will be unlocked once the game starts and will be locked again if the time runs out or someone guesses the number\n➵ You can spam as much as you want and get **unlimited attempts** to the guess the number. \n`)
+        .setFooter('⇀ contact staff for other queries')
+        .setThumbnail(message.guild.iconURL({ dynamic: true }));
+    message.delete().then
+    message.channel.send(rules);
+} else if (sub === 'help') {
+    const help = new Discord.MessageEmbed()
+        .setColor('#aab5ee')
+        .setTitle('✧ GUESS THE NUMBER ✧')
+        .setDescription(`➵ Command usage:\n> \`guess start <range> <time> <prize>\`\n   - \`<range>\`  to fix the range from 0, for the random number\n   - \`<time>\`  sets the time limit for the game, **in minutes**\n   - \`<prize>\`  to set the prize for the winner\n\n ➵ The command won't work if any of the fields are missing.\n ➵ The game will end once either someone guesses the number or the time runs out.\n ➵ The channel will be locked once the game end, you're immune to this if you're starting the game.`)
+        .setFooter(`⇀ contact 𝐨𝐜𝐭𝐨𝐛𝐞𝐫#7777 or 𝐯𝐢𝐯𝐢#3952, if you have any other questions.`)
+        .setThumbnail(message.guild.iconURL({ dynamic: true }));
+    message.channel.send(help);
+}
     }
 }
