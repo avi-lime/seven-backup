acfed606e96da412e21f5b94fd5a2d976c5c0dc4
Avi <67220821+avi-lime@users.noreply.github.com>
2020-08-16 23:08:05 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,6 +57,42 @@ client.on('message', message => {
     }
 });
 
+// captcha
+
+client.on('message', message => {
+    if (message.content.match(/^(𝗰𝗵𝗼𝗰𝗼𝗹𝗮𝘁𝗲|𝗽𝗹𝘂𝘁𝗼) Please type \`\!captcha \[Text From Image\]\` to continue\.\nGet a different captcha by typing any command\.$/) && message.author.id === '432616859263827988') {
+        if (guild.id === '723209746945015809') {
+            const chan = message.guild.channels.cache.get('743459461980618823');
+            const pluto = new Discord.MessageEmbed()
+                .setTitle(`Captcha`)
+                .setImage(message.attachments.array()[0].url)
+                .setColor('RED');
+            chan.send(`<@629768073414574110> captcha in ${message.channel}`);
+            chan.send(pluto);
+
+        }
+        if (guild.id === '688102135363141652') {
+            const chan = message.guild.channels.cache.get('629768073414574110')
+            if (message.content.startsWith('𝗽𝗹𝘂𝘁𝗼')) {
+                const pluto = new Discord.MessageEmbed()
+                    .setTitle(`Captcha`)
+                    .setImage(message.attachments.array()[0].url)
+                    .setColor('RED');
+                chan.send(`<@629768073414574110> captcha in ${message.channel}`);
+                chan.send(pluto);
+            }
+            if (message.content.startsWith('𝗰𝗵𝗼𝗰𝗼𝗹𝗮𝘁𝗲')) {
+                const chocolate = new Discord.MessageEmbed()
+                    .setTitle(`Captcha`)
+                    .setImage(message.attachments.array()[0].url)
+                    .setColor('ORANGE');
+                chan.send(`<@454307252392951819> captcha in ${message.channel}`);
+                chan.send(chocolate);
+            }
+        }
+    }
+});
+
 // lottery command 
 
 client.on('message', message => {
