43b76d03493f2bb381f88b737fea40e0c38e1857
Avi <67220821+avi-lime@users.noreply.github.com>
2020-08-16 16:48:18 +0530

    made lockverse and unlockverse shorter with forEach method

--- commands/unlockverse.js
+++ commands/unlockverse.js
@@ -5,6 +5,7 @@ module.exports = {
     name: 'unlockverse',
     description: 'unlocks all adventure channels',
     execute(msg) {
+        const raidchannels = ['735089552326393957', '735089631871238155', '735089717703606345', '735089719712677919', '735089800574664715', '735089802944446474', '735791071115870271', '735802535272775710', '735802613563523142', '735802656588562477'];
         const args = msg.content.slice(prefix.length).trim().split(/ +/);
         const commandName = args.shift().toLowerCase();
         const everyone = msg.channel.guild.roles.everyone;
@@ -16,38 +17,14 @@ module.exports = {
             .setTimestamp()
             .setColor('GREEN');
 
-        const adv1 = msg.guild.channels.cache.get('735089552326393957');
-        const adv2 = msg.guild.channels.cache.get('735089631871238155');
-        const adv3 = msg.guild.channels.cache.get('735089717703606345');
-        const adv4 = msg.guild.channels.cache.get('735089719712677919');
-        const adv5 = msg.guild.channels.cache.get('735089800574664715');
-        const adv6 = msg.guild.channels.cache.get('735089802944446474');
-        const adv7 = msg.guild.channels.cache.get('735791071115870271');
-        const adv8 = msg.guild.channels.cache.get('735802535272775710');
-        const adv9 = msg.guild.channels.cache.get('735802613563523142');
-        const adv10 = msg.guild.channels.cache.get('735802656588562477');
-
         if (!msg.member.roles.cache.some(r => ['✈ 守護天使 — angi', '❦ 管理人 — admin'].includes(r.name))) return;
-        adv1.updateOverwrite(everyone, { SEND_MESSAGES: null }).then
-        adv1.send(unlocked).then
-        adv2.updateOverwrite(everyone, { SEND_MESSAGES: null }).then
-        adv2.send(unlocked).then
-        adv3.updateOverwrite(everyone, { SEND_MESSAGES: null }).then
-        adv3.send(unlocked).then
-        adv4.updateOverwrite(everyone, { SEND_MESSAGES: null }).then
-        adv4.send(unlocked).then
-        adv5.updateOverwrite(everyone, { SEND_MESSAGES: null }).then
-        adv5.send(unlocked).then
-        adv6.updateOverwrite(everyone, { SEND_MESSAGES: null }).then
-        adv6.send(unlocked).then
-        adv7.updateOverwrite(everyone, { SEND_MESSAGES: null }).then
-        adv7.send(unlocked).then
-        adv8.updateOverwrite(everyone, { SEND_MESSAGES: null }).then
-        adv8.send(unlocked).then
-        adv9.updateOverwrite(everyone, { SEND_MESSAGES: null }).then
-        adv9.send(unlocked).then
-        adv10.updateOverwrite(everyone, { SEND_MESSAGES: null }).then
-        adv10.send(unlocked).then
+
+        raidchannels.forEach((channel) => {
+            msg.guild.channels.cache.get(channel).updateOverwrite(everyone, { SEND_MESSAGES: null }).then(chan => {
+                chan.send(unlocked);
+            })
+        });
+
         msg.channel.send(`<a:sevenloading:739558030420475904> unlocking channels...`).then(m => {
             setTimeout(function () {
                 m.edit(`<a:sevensuccess:740956620678234222> all channels have been unlocked!`)
