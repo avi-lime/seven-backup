8de08693aa702af81e18bc4a8f7717ffca36e0ea
angi-mint <68991185+angi-mint@users.noreply.github.com>
2020-09-01 20:30:25 +0200

    added automatic powerbar after giveaways

---
+++ autoresponses/powerbar.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('powerbar working');
+})
+client.on('message', message => {
+       if(message.content.match(/https\:\/\/discordapp.com\/channels\//)) {
+         if (message.author.bot) {
+             message.channel.send("✩̣̣̣┄•͙✧⃝•͙┄✩ͯ•͙͙✧⃝•͙͙✩ͯ┄•͙✧⃝•͙┄✩̣̣̣");
+        }
+    }
+});
+
+client.login(process.env.token);
