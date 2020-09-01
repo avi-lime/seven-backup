e0c4a597765e3d75f03f7ebd70f27c552b7cc1c5
Avi <67220821+avi-lime@users.noreply.github.com>
2020-09-01 16:28:54 +0530

    Update hello.js

--- autoresponses/hello.js
+++ autoresponses/hello.js
@@ -4,11 +4,12 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('hello ar working');
 })
-
-if (message.content === 'hello') {
-    if (!message.author.bot) {
-        message.channel.send("안녕하세요!");
+client.on('message', message => {
+    if (message.content === 'hello') {
+        if (!message.author.bot) {
+            message.channel.send("안녕하세요!");
+        }
     }
-}
+});
 
 client.login(process.env.token);
