840bb84d45eaadcbf815d4bad9b6634e3d4665ca
Avi <67220821+avi-lime@users.noreply.github.com>
2020-09-02 16:49:47 +0530

    Update lottery.js

--- autoresponses/lottery.js
+++ autoresponses/lottery.js
@@ -16,28 +16,29 @@ client.on('message', message => {
     const sub = args[0];
     const lotteryChan = ['688109298852692055', '688396273723637807'];
     const logchan = client.guilds.cache.get('739554666752376984').channel.cache.get('750663554901737503');
-
-    if (sub === 'start') {
-        if (!lotteryChan.includes(message.channel.id) || !message.member.roles.cache.has('735069864636710923')) return;
-        hostid = message.author.id;
-        const started = new Discord.MessageEmbed()
-            .setTitle(`<:seventickets:750410697233662052> :: LOTTERY × <:seventickets:750410697233662052>`)
-            .setDescription(`the lottery has been started!`)
-            .addFields({ name: '<:seventicket:750410632318156900> ticket amount', value: '10,000 dmc' }, { name: '<a:sevenrich:750415401694920727> participate', value: `to participate pay the ticket prize to ${message.author}` })
-            .setColor(message.member.displayHexColor)
-            .setTimestamp()
-            .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
-            .setFooter('🍓 good luck', message.guild.iconURL({ dynamic: true }));
-        message.channel.send(started);
-        const logstart = new Discord.MessageEmbed()
-            .setTitle(`Lottery Started!`)
-            .setDescription(`Lottery started in ${message.channel} by ${message.author}`)
-            .setColor(message.member.displayHexColor)
-            .setThumbnail(message.author.displayAvatarURL({ dynamic: true }));
-        logchan.send(logstart)
-    }
-    if (sub === 'show') {
-        message.channel.send(`the host is <@${hostid}>`);
+    if (commandName === 'lottery') {
+        if (sub === 'start') {
+            if (!lotteryChan.includes(message.channel.id) || !message.member.roles.cache.has('735069864636710923')) return;
+            hostid = message.author.id;
+            const started = new Discord.MessageEmbed()
+                .setTitle(`<:seventickets:750410697233662052> :: LOTTERY × <:seventickets:750410697233662052>`)
+                .setDescription(`the lottery has been started!`)
+                .addFields({ name: '<:seventicket:750410632318156900> ticket amount', value: '10,000 dmc' }, { name: '<a:sevenrich:750415401694920727> participate', value: `to participate pay the ticket prize to ${message.author}` })
+                .setColor(message.member.displayHexColor)
+                .setTimestamp()
+                .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
+                .setFooter('🍓 good luck', message.guild.iconURL({ dynamic: true }));
+            message.channel.send(started);
+            const logstart = new Discord.MessageEmbed()
+                .setTitle(`Lottery Started!`)
+                .setDescription(`Lottery started in ${message.channel} by ${message.author}`)
+                .setColor(message.member.displayHexColor)
+                .setThumbnail(message.author.displayAvatarURL({ dynamic: true }));
+            logchan.send(logstart)
+        }
+        if (sub === 'show') {
+            message.channel.send(`the host is <@${hostid}>`);
+        }
     }
 })
 
