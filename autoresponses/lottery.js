55698d23e03601c54d005dfdf0d7eb852f74c19a
Avi <67220821+avi-lime@users.noreply.github.com>
2020-09-02 19:58:43 +0530

    Update lottery.js

--- autoresponses/lottery.js
+++ autoresponses/lottery.js
@@ -20,7 +20,7 @@ client.on('message', message => {
         if (message.member.roles.cache.get('750657899847614476')) {
             return message.channel.send(`you've already participated in the lottery!`);
         } else {
-            prize = Math.floor[100 + `${lotteryrole.members.size}` * 10];
+            prize = 100 + lotteryrole.size * 10;
             const joined = new Discord.MessageEmbed()
                 .setTitle(`Lottery Joined!`)
                 .setDescription(`> <:seventicket:750410632318156900> You've successfully joined the lottery\n  × current prize: **${prize}k**\n  × I've added the lottery role to you, winner will be announced soon`)
@@ -72,12 +72,12 @@ client.on('message', message => {
             status = false;
         }
         if (sub === 'show') {
-            prize = Math.floor[100 + `${lotteryrole.members.size}` * 10];
+            prize = 100 + lotteryrole.size * 10;
             if (lotteryrole.members.size <= 10) {
                 const show = new Discord.MessageEmbed()
                     .setTitle(`Lottery Stats`)
                     .setColor(message.member.displayHexColor)
-                    .addFields({ name: 'Host', value: `<@${hostid}>` }, { name: `Participants`, value: lotteryrole.members.map().split("\n") }, { name: `Prize`, value: `${prize}k` });
+                    .addFields({ name: 'Host', value: `<@${hostid}>` }, { name: `Participants`, value: lotteryrole.members }, { name: `Prize`, value: `${prize}k` });
                 message.channel.send(show);
             } else {
                 const show = new Discord.MessageEmbed()
