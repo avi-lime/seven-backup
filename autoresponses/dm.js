20228fba95e44b32985286fadbd0aac848a3d301
Avi <67220821+avi-lime@users.noreply.github.com>
2020-09-04 21:21:04 +0530

    added .catch

--- autoresponses/dm.js
+++ autoresponses/dm.js
@@ -23,14 +23,14 @@ client.on('message', message => {
         if (!message.member.roles.cache.has('688388235981881374')) return;
         const toSend = message.mentions.members.first();
         if (toSend) {
-            toSend.send(message.content.replace(prefix + commandName + ' ' + args[0], ''));
+            toSend.send(message.content.replace(prefix + commandName + ' ' + args[0], '')).catch(message.channel.send(`can't send a dm to this user`));
             message.delete;
             message.channel.send(`message sent to ${toSend.displayName}`);
         } else if (message.mentions.users.size === 0) {
             const userid = args[0].toString();
             const user = message.guild.members.cache.get(userid);
             if (user) {
-                user.send(message.content.replace(prefix + commandName + ' ' + args[0], ''));
+                user.send(message.content.replace(prefix + commandName + ' ' + args[0], '')).catch(message.channel.send(`can't send a dm to this user`));
                 message.delete;
                 message.channel.send(`message sent to ${user.displayName}`);
             } else return message.channel.send(`No user found, either mention a proper user or use their ID`);
@@ -41,14 +41,14 @@ client.on('message', message => {
         if (!message.member.roles.cache.has('688388235981881374')) return;
         const toSend = message.mentions.members.first();
         if (toSend) {
-            toSend.send(`${message.author.tag}: ${message.content.replace(prefix + commandName + ' ' + args[0], '')}`);
+            toSend.send(`${message.author.tag}: ${message.content.replace(prefix + commandName + ' ' + args[0], '')}`).catch(message.channel.send(`can't send a dm to this user`));
             message.delete;
             message.channel.send(`message sent to ${toSend.displayName}`);
         } else if (message.mentions.users.size === 0) {
             const userid = args[0].toString();
             const user = message.guild.members.cache.get(userid);
             if (user) {
-                user.send(`${message.author.tag}: ${message.content.replace(prefix + commandName + ' ' + args[0], '')}`);
+                user.send(`${message.author.tag}: ${message.content.replace(prefix + commandName + ' ' + args[0], '')}`).catch(message.channel.send(`can't send a dm to this user`));
                 message.delete;
                 message.channel.send(`message sent to ${user.displayName}`);
             } else return message.channel.send(`No user found, either mention a proper user or use their ID`);
