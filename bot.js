b646865e18aba1ffca4c3bc43fb274dc515f0b99
Avi <67220821+avi-lime@users.noreply.github.com>
2020-09-04 19:10:16 +0530

    removed dm and cp from bot.js

--- bot.js
+++ bot.js
@@ -51,111 +51,17 @@ client.on('message', message => {
 });
 
 // captcha
+captcha = require('./autoresponses/captcha');
 
-client.on('message', message => {
-    if (message.content.match(/^(𝗰𝗵𝗼𝗰𝗼𝗹𝗮𝘁𝗲|𝗽𝗹𝘂𝘁𝗼) Please type \`\!captcha \[Text From Image\]\` to continue\.\nGet a different captcha by typing any command\.$/) && message.author.id === '432616859263827988') {
-        if (message.guild.id === '723209746945015809') {
-            const chan = message.guild.channels.cache.get('743459461980618823');
-            if (message.content.startsWith('𝗽𝗹𝘂𝘁𝗼')) {
-                const pluto = new Discord.MessageEmbed()
-                    .setTitle(`Captcha`)
-                    .setImage(message.attachments.array()[0].url)
-                    .setColor('RED');
-                chan.send(`<@629768073414574110> captcha in ${message.channel}`);
-                chan.send(pluto);
-            }
-            if (message.content.startsWith('𝗰𝗵𝗼𝗰𝗼𝗹𝗮𝘁𝗲')) {
-                const chocolate = new Discord.MessageEmbed()
-                    .setTitle(`Captcha`)
-                    .setImage(message.attachments.array()[0].url)
-                    .setColor('ORANGE');
-                chan.send(`<@454307252392951819> captcha in ${message.channel}`);
-                chan.send(chocolate);
-            }
+// lottery command 
+lottery = require('./autoresponses/lottery');
 
-        }
-        if (message.guild.id === '688102135363141652') {
-            const chan = message.guild.channels.cache.get('688109298852692055')
-            if (message.content.startsWith('𝗽𝗹𝘂𝘁𝗼')) {
-                const pluto = new Discord.MessageEmbed()
-                    .setTitle(`Captcha`)
-                    .setImage(message.attachments.array()[0].url)
-                    .setColor('RED');
-                chan.send(`<@629768073414574110> captcha in ${message.channel}`);
-                chan.send(pluto);
-            }
-            if (message.content.startsWith('𝗰𝗵𝗼𝗰𝗼𝗹𝗮𝘁𝗲')) {
-                const chocolate = new Discord.MessageEmbed()
-                    .setTitle(`Captcha`)
-                    .setImage(message.attachments.array()[0].url)
-                    .setColor('ORANGE');
-                chan.send(`<@454307252392951819> captcha in ${message.channel}`);
-                chan.send(chocolate);
-            }
-        }
-    }
+// Powerbar
+powerbar = require('./autoresponses/powerbar');
 
-    // lottery command 
+// DM channel
+dm = require('./autoresponses/dm');
 
-    lottery = require('./autoresponses/lottery');
-
-    // Powerbar
-    powerbar = require('./autoresponses/powerbar');
-
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const commandName = args.shift().toLowerCase();
-    const sub = args[0];
-    const lc = ['688396273723637807'];
-
-    // DM channel
-
-    // if someone sends the DM, it redirects to the staff-bot1 channel.
-    if (message.channel.type === 'dm') {
-        if (message.author.bot) return;
-        const usermsg = new Discord.MessageEmbed()
-            .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
-            .setColor("RED")
-            .setDescription(message.content)
-            .setTimestamp()
-            .setFooter(message.author.id);
-        client.guilds.cache.get('688102135363141652').channels.cache.get('688109298852692055').send(usermsg);
-    }
-    if (commandName === 'dm' || commandName === 'reply') {
-        if (!message.content.startsWith(prefix) || message.author.bot) return;
-        const toSend = message.mentions.members.first();
-        if (toSend) {
-            toSend.send(message.content.replace(prefix + commandName + ' ' + args[0], ''));
-            message.delete;
-            message.channel.send(`message sent to ${toSend.displayName}`);
-        } else if (message.mentions.users.size === 0) {
-            const userid = args[0].toString();
-            const user = message.guild.members.cache.get(userid);
-            if (user) {
-                user.send(message.content.replace(prefix + commandName + ' ' + args[0], ''));
-                message.delete;
-                message.channel.send(`message sent to ${user.displayName}`);
-            } else return message.channel.send(`No user found, either mention a proper user or use their ID`);
-        }
-    }
-    if (commandName === 'dmw' || commandName === 'replyw') {
-        if (!message.content.startsWith(prefix) || message.author.bot) return;
-        const toSend = message.mentions.members.first();
-        if (toSend) {
-            toSend.send(`${message.author.tag}: ${message.content.replace(prefix + commandName + ' ' + args[0], '')}`);
-            message.delete;
-            message.channel.send(`message sent to ${toSend.displayName}`);
-        } else if (message.mentions.users.size === 0) {
-            const userid = args[0].toString();
-            const user = message.guild.members.cache.get(userid);
-            if (user) {
-                user.send(`${message.author.tag}: ${message.content.replace(prefix + commandName + ' ' + args[0], '')}`);
-                message.delete;
-                message.channel.send(`message sent to ${user.displayName}`);
-            } else return message.channel.send(`No user found, either mention a proper user or use their ID`);
-        }
-    }
-});
 
 
 /*client.on('guildMemberAdd', member =>  {
