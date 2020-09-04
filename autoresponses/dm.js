427a2e306283b4d8accce86a80a2dd399048aaab
Avi <67220821+avi-lime@users.noreply.github.com>
2020-09-04 22:41:34 +0530

    .catch() for discordapierror

--- autoresponses/dm.js
+++ autoresponses/dm.js
@@ -23,25 +23,21 @@ client.on('message', message => {
         if (!message.member.roles.cache.has('688388235981881374')) return;
         const toSend = message.mentions.members.first();
         if (toSend) {
-            try {
-                toSend.send(message.content.replace(prefix + commandName + ' ' + args[0], ''));
-                message.delete;
-                message.channel.send(`message sent to ${toSend.displayName}`);
-            } catch (error) {
-                if (error === Discord.DiscordAPIError) return message.channel.send(`can't send a dm to this user`);
-            };
+            toSend.send(message.content.replace(prefix + commandName + ' ' + args[0], '')).catch(e => {
+                if (e === Discord.DiscordAPIError) return message.channel.send(`can't send a dm to this user`);
+            });
+            message.delete();
+            message.channel.send(`message sent to ${toSend.displayName}`);
+
         } else if (message.mentions.users.size === 0) {
             const userid = args[0].toString();
             const user = message.guild.members.cache.get(userid);
             if (user) {
-                try {
-                    user.send(message.content.replace(prefix + commandName + ' ' + args[0], ''));
-                    message.delete;
-                    message.channel.send(`message sent to ${user.displayName}`);
-                } catch (error) {
-                    if (error === Discord.DiscordAPIError) return message.channel.send(`can't send a dm to this user`);
-                };
-
+                user.send(message.content.replace(prefix + commandName + ' ' + args[0], '')).catch(e => {
+                    if (e === Discord.DiscordAPIError) return message.channel.send(`can't send a dm to this user`);
+                });
+                message.delete();
+                message.channel.send(`message sent to ${user.displayName}`);
             } else return message.channel.send(`No user found, either mention a proper user or use their ID`);
         }
     }
@@ -50,25 +46,20 @@ client.on('message', message => {
         if (!message.member.roles.cache.has('688388235981881374')) return;
         const toSend = message.mentions.members.first();
         if (toSend) {
-            try {
-                toSend.send(`${message.author.tag}: ${message.content.replace(prefix + commandName + ' ' + args[0], '')}`);
-                message.delete;
-                message.channel.send(`message sent to ${toSend.displayName}`);
-            } catch (error) {
-                if (error === Discord.DiscordAPIError) return message.channel.send(`can't send a dm to this user`);
-            };
-
+            toSend.send(`${message.author.tag}: ${message.content.replace(prefix + commandName + ' ' + args[0], '')}`).catch(e => {
+                if (e === Discord.DiscordAPIError) return message.channel.send(`can't send a dm to this user`);
+            });
+            message.delete;
+            message.channel.send(`message sent to ${toSend.displayName}`);
         } else if (message.mentions.users.size === 0) {
             const userid = args[0].toString();
             const user = message.guild.members.cache.get(userid);
             if (user) {
-                try {
-                    user.send(`${message.author.tag}: ${message.content.replace(prefix + commandName + ' ' + args[0], '')}`);
-                    message.delete;
-                    message.channel.send(`message sent to ${user.displayName}`);
-                } catch (error) {
-                    if (error === Discord.DiscordAPIError) return message.channel.send(`can't send a dm to this user`);
-                };
+                user.send(`${message.author.tag}: ${message.content.replace(prefix + commandName + ' ' + args[0], '')}`).catch(e => {
+                    if (e === Discord.DiscordAPIError) return message.channel.send(`can't send a dm to this user`);
+                });;
+                message.delete;
+                message.channel.send(`message sent to ${user.displayName}`);
 
             } else return message.channel.send(`No user found, either mention a proper user or use their ID`);
         }
