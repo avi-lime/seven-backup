f13e5c1862e72045ee87ef5749dfc2248d5cd66a
Avi <67220821+avi-lime@users.noreply.github.com>
2020-09-07 16:33:21 +0530

    added -gwm guide

--- commands/gwm.js
+++ commands/gwm.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const prefix = '-';
-
+const client = new Discord.Client();
 module.exports = {
     name: 'gwm',
     description: 'adds the mentioned user to giveway manager roles',
@@ -8,6 +8,7 @@ module.exports = {
         const args = msg.content.slice(prefix.length).trim().split(/ +/);
         const user = msg.mentions.members.first();
         const sub = args[1];
+        const subCommands = ['fix', 'guide'];
         if (!msg.member.roles.cache.some(r => ['✈ 守護天使 — angi', '❦ 管理人 — admin', '.•° head mod  °•.'].includes(r.name))) return;
         if (!user) {
             return msg.channel.send('mention someone to appoint as giveaway manager');
@@ -24,7 +25,8 @@ module.exports = {
                 }, 1500)
             });
             return;
-        } else if (sub !== 'fix') {
+        }
+        if (!subCommands.includes(sub)) {
             const guide = new Discord.MessageEmbed()
                 .setTitle('Congratulations on being promoted to giveaway manager in your eyes tell :sparkles:')
                 .setAuthor(msg.mentions.users.first().tag, msg.mentions.users.first().displayAvatarURL({ dynamic: true }))
@@ -40,6 +42,30 @@ module.exports = {
                 }, 1500)
             }).then
             user.send(guide);
+        } else if (sub === 'guide') {
+            if (args[1] === 'all') {
+                const gwmrole = client.guilds.cache.get('688102135363141652').roles.cache.get('688386729807577284');
+                const guide = new Discord.MessageEmbed()
+                    .setTitle(`Giveaway guide for ${msg.guild.name}`)
+                    .setColor('#aab5ee')
+                    .setDescription(`here's a guide on how to make giveaways & a few tips\n\n> **how to start a giveaway** \n> using <@294882584201003009>: \n \`g!start (time) (winners) (prize)\`\n example: \`g!start 4h 1w 50k OwO cash\`\n> using <@700743797977514004>:\n \`+gstart (time) (winner) (req) (prize)\`\n example: \`+gstart 10m 1w none 100k dmc\` || \`+gstart 10m 1w 1[amari] 100k dmc\`\n\n> **how to ping the different giveaway roles** (using <@737933523918389328> )\n \`-gw <type> <donator> <message>\` [<message> is optional & **not pinging anyone means you're the donator**]\n __command        | role it will ping__\n \`-gw\`                    | 景品 — giveaway ping\n \`-gw verse\`      | 景品 — pokéverse giveaway \n \`-gw myuu\`        | 景品 — myuu giveaway\n \`-gw pokemon\` | ポケモン — pokemon access \n\n> **handing out prizes in the main bots:**\n- dank memer: pls share (@user) (amount)\n- owo: owo give (@user) (amount)\n- pokeverse: !pay (@user) (amount)\n- myuu: .gift (@user) (amount)\n\n> **additional notes:**\n- make sure to **take all donations** ( <#738458175187058698> )\n- please **do not enter your own giveaway**\n- keep talking to a minimum and **only talk about giveaways** in the <#737298442434379867> and <#738458175187058698> channel\n- don't forget the **handout of the rewards** ( <#737298442434379867> ), either set a reminder or keep all notifications on for the channel\n- **after a giveaway** has finished use \`;pb\` (pb for powerbar, it'll post a divider)\n- you can **host more than one giveaway** at a time, not more than 5 at a time though\n- in the giveaway always mention in what bot the rewards will be, for example say 50k OwO cowoncy, not just 50k\n`)
+                    .setFooter('🌼 made by angel for her lovely giveaway managers ♡', msg.guild.iconURL({ dynamic: true }));
+
+                gwmrole.members.forEach(member => {
+                    member.send(guide);
+                }).then
+                msg.channel.send(`guide sent to all the giveaway managers!`);
+            } else {
+                if (!user) return msg.channel.send(`either mention someone to send the guide to, or do \`-gwm guide all\` to send to everyone with giveaway manager role`);
+                const guide = new Discord.MessageEmbed()
+                    .setTitle(`Giveaway guide for ${msg.guild.name}`)
+                    .setAuthor(msg.mentions.users.first().tag, msg.mentions.users.first().displayAvatarURL({ dynamic: true }))
+                    .setColor(user.displayHexColor)
+                    .setDescription(`here's a guide on how to make giveaways & a few tips\n\n> **how to start a giveaway** \n> using <@294882584201003009>: \n \`g!start (time) (winners) (prize)\`\n example: \`g!start 4h 1w 50k OwO cash\`\n> using <@700743797977514004>:\n \`+gstart (time) (winner) (req) (prize)\`\n example: \`+gstart 10m 1w none 100k dmc\` || \`+gstart 10m 1w 1[amari] 100k dmc\`\n\n> **how to ping the different giveaway roles** (using <@737933523918389328> )\n \`-gw <type> <donator> <message>\` [<message> is optional & **not pinging anyone means you're the donator**]\n __command        | role it will ping__\n \`-gw\`                    | 景品 — giveaway ping\n \`-gw verse\`      | 景品 — pokéverse giveaway \n \`-gw myuu\`        | 景品 — myuu giveaway\n \`-gw pokemon\` | ポケモン — pokemon access \n\n> **handing out prizes in the main bots:**\n- dank memer: pls share (@user) (amount)\n- owo: owo give (@user) (amount)\n- pokeverse: !pay (@user) (amount)\n- myuu: .gift (@user) (amount)\n\n> **additional notes:**\n- make sure to **take all donations** ( <#738458175187058698> )\n- please **do not enter your own giveaway**\n- keep talking to a minimum and **only talk about giveaways** in the <#737298442434379867> and <#738458175187058698> channel\n- don't forget the **handout of the rewards** ( <#737298442434379867> ), either set a reminder or keep all notifications on for the channel\n- **after a giveaway** has finished use \`;pb\` (pb for powerbar, it'll post a divider)\n- you can **host more than one giveaway** at a time, not more than 5 at a time though\n- in the giveaway always mention in what bot the rewards will be, for example say 50k OwO cowoncy, not just 50k\n`)
+                    .setFooter('🌼 made by angel for her lovely giveaway managers ♡', msg.guild.iconURL({ dynamic: true }));
+                user.send(guide);
+                msg.channel.send(`guide sent to ${user}`);
+            }
         }
     }
 }
