cc1bb2f4317903616e1f8b64d95bd7582a4cf3cc
Avi <67220821+avi-lime@users.noreply.github.com>
2020-09-11 14:19:04 +0530

    questions for application

---
+++ application/questions.js
@@ -0,0 +1,9 @@
+module.exports = [
+    `1. What is your name/nickname (what you'd like to be called in the server)?`,
+    `2. What is your age (and gender)?`,
+    `3. In which time zone do you live?`,
+    `4. Are you ready to take donations at any time (that you're online or get pinged) and host giveaways?`,
+    `5. Do you have any experience as a Giveaway Manager in any other servers? If yes then which servers? If no then why do you think you are capable to work with us and why did you choose our server?`,
+    `6. The daily requirement of the giveaways is 50k, will you be able to fulfill the requirements?`,
+    `7. Lastly, How long have you been playing the Dank Memer?`
+];
