0590bf66d4db6e08652242fd944ae0ab493c9979
Avi <67220821+avi-lime@users.noreply.github.com>
2020-09-12 21:17:47 +0530

    Create team.js

---
+++ commands/team.js
@@ -0,0 +1,46 @@
+const prefix = '-';
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+module.exports = {
+    name: 'team',
+    description: 'to view team stats and list of members',
+    execute(message) {
+        var teamAngi = client.guilds.cache.get('688102135363141652').roles.cache.get('735606747481374770');
+        var teamAvi = client.guilds.cache.get('688102135363141652').roles.cache.get('735608688441557055');
+
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+        const commandName = args.shift().toLowerCase();
+        const subCommands = ['view', 'stats'];
+
+        if (!args[0] || !subCommands.includes(args[0])) {
+            return message.channel.send(`available commands are\n\`${prefix}team view\` and \`${prefix}team stats\``);
+        } else if (args[0] === 'view') {
+            const teams = ['avi', 'angi'];
+            if (!args[1] || !teams.includes(args[1])) {
+                message.channel.send(`available teams to view are \`angi\` and \`avi\``);
+            } else if (args[1] === 'avi') {
+                const avi = new Discord.MessageEmbed()
+                    .setTitle(`Team Avi`)
+                    .addFields({ name: `Team`, value: `<@&735608688441557055>` }, { name: `Members`, value: teamAvi.members.map(m => m.user.tag).join('\n') })
+                    .setColor('ORANGE')
+                    .setFooter(`Total Members: ${teamAvi.members.size}`);
+                message.channel.send(avi);
+            } else if (args[1] === 'angi') {
+                const angi = new Discord.MessageEmbed()
+                    .setTitle(`Team Angi`)
+                    .addFields({ name: `Team`, value: `<@&735606747481374770>` }, { name: `Members`, value: teamAngi.members.map(m => m.user.tag).join('\n') })
+                    .setColor('RED')
+                    .setFooter(`Total Members: ${teamAngi.members.size}`);
+                message.channel.send(angi)
+            }
+        } else if (args[0] === 'stats') {
+            const teamStats = new Discord.MessageEmbed()
+                .setTitle(`Team Statistics`)
+                .addFields({ name: `Team Angi`, value: `Members: ${teamAngi.members.size}` }, { name: `Team Avi`, value: `Member: ${teamAvi.members.size}` })
+                .setColor(message.member.displayHexColor)
+                .setFooter(`team competitions soon~`);
+            message.channel.send(teamStats);
+        }
+    }
+}
