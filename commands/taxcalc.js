7bf0aeb268bb58c1459ecf1f11395cc34a4d8301
Avi <67220821+avi-lime@users.noreply.github.com>
2020-09-19 18:16:43 +0530

    added scientific conversions and corrected tax calculation

--- commands/taxcalc.js
+++ commands/taxcalc.js
@@ -16,12 +16,17 @@ module.exports = {
                 return parseFloat(val) * 1000000;
             else if (!isNaN(val))
                 return parseFloat(val);
+            else if (multiplier == 'e') {
+                power = val.splice(0, -1);
+                Math.pow(parseFloat(val), power);
+                return;
+            }
             else return message.channel.send(`Give me a valid number`);
         }
 
         const args = message.content.slice(prefix.length).trim().split(/ +/);
         const commandName = args.shift().toLowerCase();
-        var tax, amtToBePaid, taxPaid;
+        var tax, amtToBePaid, taxPaid, finalAmt;
         if (!args[0]) return message.channel.send(`Give me a number to calculate tax for`);
         const amount = getVal(args[0]);
 
@@ -29,28 +34,48 @@ module.exports = {
         if (amount < 20001) {
             tax = 0;
             amtToBePaid = amount;
+            finalAmt = amtToBePaid;
             taxPaid = 0;
         } else if (amount > 20000 && amount < 50001) {
             tax = 1;
             amtToBePaid = Math.round(amount * 1.010101);
-            taxPaid = Math.round(amount * 0.010101);
+            if (amtToBePaid > 50000) {
+                taxPaid = Math.round(amount * 0.030927);
+                finalAmt = Math.round(amount * 1.030927)
+            } else {
+                finalAmt = amtToBePaid;
+                taxPaid = Math.round(amount * 1.010101);
+            }
         } else if (amount > 50000 && amount < 500001) {
             tax = 3;
             amtToBePaid = Math.round(amount * 1.030927);
-            taxPaid = Math.round(amount * 0.030927);
+            if (amtToBePaid > 500000) {
+                taxPaid = Math.round(amount * 0.052625);
+                finalAmt = Math.round(amount * 1.052625);
+            } else {
+                finalAmt = amtToBePaid;
+                taxPaid = Math.round(amount * 0.030927);
+            }
         } else if (amount > 500000 && amount < 1000001) {
             tax = 5;
             amtToBePaid = Math.round(amount * 1.052625);
-            taxPaid = Math.round(amount * 0.052625);
+            if (amtToBePaid > 1000000) {
+                finalAmt = Math.round(amount * 1.086912);
+                taxPaid = Math.round(amount * 0.086912);
+            } else {
+                finalAmt = amtToBePaid;
+                taxPaid = Math.round(amount * 0.052625);
+            }
         } else if (amount > 1000000) {
             tax = 8;
             amtToBePaid = Math.round(amount * 1.086912);
+            finalAmt = amtToBePaid;
             taxPaid = Math.round(amount * 0.086912);
         }
         const taxEmbed = new Discord.MessageEmbed()
             .setTitle(`Tax Calculator`)
             .setDescription(`Tax Calculated for **${amount.toLocaleString()}**`)
-            .addFields({ name: `Amount to be Paid`, value: amtToBePaid.toLocaleString() }, { name: `Tax Paid`, value: taxPaid.toLocaleString(), inline: true }, { name: `Tax`, value: `${tax}%`, inline: true })
+            .addFields({ name: `Amount to be Paid`, value: finalAmt.toLocaleString() }, { name: `Tax Paid`, value: taxPaid.toLocaleString(), inline: true }, { name: `Tax`, value: `${tax}%`, inline: true })
             .setFooter(`💖 made for the lovely members of your eyes tell ✨`, message.guild.iconURL({ dynamic: true }))
             .setColor(message.member.displayHexColor)
             .setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic: true }));
