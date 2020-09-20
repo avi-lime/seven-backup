9d282ade55b263e5487c8bd2acd736601b4ac4ae
Avi <67220821+avi-lime@users.noreply.github.com>
2020-09-20 15:12:27 +0530

    smol change in embed

--- commands/embed.js
+++ commands/embed.js
@@ -10,7 +10,7 @@ module.exports = {
             .setFooter(`for more booster perks check out the booster channel`)
             .setThumbnail(message.guild.iconURL({ dynamic: true }))
             .setTitle(`<:sevenboost:757160467226558536> ticket system :: private channel <:sevenboost:757160467226558536>`)
-            .setDescription(`<a:sevenbooster:757147160684003399> thank you for boosting our server!\n     you can now create your own private channel by reacting on <a:sevenbooster:757147160684003399>`);
+            .setDescription(`<a:sevenbooster:757147160684003399> **thank you for boosting** our server!\n     you can now create your **own private channel** by reacting on <a:sevenbooster:757147160684003399>`);
         message.delete().then
         message.channel.send(Embed).then(sentMessage => {
             sentMessage.react('757147160684003399');
