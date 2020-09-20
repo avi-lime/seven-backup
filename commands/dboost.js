5c534ec3095cd8f4c9535cc17d1a85bce1b9ae5c
Avi <67220821+avi-lime@users.noreply.github.com>
2020-09-20 20:57:06 +0530

    gei

--- commands/dboost.js
+++ commands/dboost.js
@@ -12,7 +12,7 @@ module.exports = {
         const rolesToAdd = ['749638466618523778', '757190782632329286', '735378963844694098'];
         const dm = new Discord.MessageEmbed()
             .setTitle(`<:sevenboost:757160467226558536> Thanks for boosting`)
-            .setDescription(`> You've been given the <@&749638466618523778> role.\n - You can ping an admin or owner to avail your booster perks if you haven't already.\n - You can get a list of all the perks in <#756925452748193843>\n - You can get your private channel from <#757140358734807120>`)
+            .setDescription(`> You've been given the **Double Server Booster** role.\n - You can ping an admin or owner to avail your booster perks if you haven't already.\n - You can get a list of all the perks in <#756925452748193843>\n - You can get your private channel from <#757140358734807120>`)
             .setColor(booster.displayHexColor)
             .setFooter(`Thanks again for boosting, enjoy your booster perks and have fun!`)
             .setAuthor(boosterUser.username, boosterUser.displayAvatarURL({ dynamic: true }));
