3fb4b002d433dde7c9c6c2cd6977e8212bf3a78a
Avi <67220821+avi-lime@users.noreply.github.com>
2020-09-20 14:20:11 +0530

    embed for ticket system

---
+++ commands/embed.js
@@ -0,0 +1,19 @@
+const prefix = '-';
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'embed',
+    description: 'embed creator to make an embed',
+    execute(message) {
+        const Embed = new Discord.MessageEmbed()
+            .setColor('#FF32F8')
+            .setFooter(`for more booster perks check out the booster channel`)
+            .setThumbnail(message.guild.iconURL({ dynamic: true }))
+            .setTitle(`<:sevenboost:757160467226558536> ticket system :: private channel <:sevenboost:757160467226558536>`)
+            .setDescription(`<a:sevenbooster:757147160684003399> thank you for boosting our server!\n     you can now create your own private channel by reacting on <a:sevenbooster:757147160684003399>`);
+        message.delete().then
+        message.channel.send(Embed).then(sentMessage => {
+            sentMessage.react('757147160684003399');
+        });
+    }
+}
