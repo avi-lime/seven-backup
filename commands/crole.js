6d24f32ba6825de823c6afd686fb19a2572f56d6
Avi <67220821+avi-lime@users.noreply.github.com>
2020-09-20 20:08:26 +0530

    idk what's wrong with this tbh

--- commands/crole.js
+++ commands/crole.js
@@ -7,11 +7,11 @@ module.exports = {
     description: 'creates a role with a specific hex color',
     execute(message) {
         const allowedRoles = ['732910738334548031', '751518094844887142', '688388305938677939'];
-        if (!message.member.roles.cache.has(r => allowedRoles.includes(r.id))) return;
+        if (!message.member.roles.cache.has(r => allowedRoles.includes(r.id))) return message.channel.send(`you don't have perms to create a role`);
         const args = message.content.slice(prefix.length).trim().split(/ +/);
         const commandName = args.shift().toLowerCase();
-        const roleName = args[0];
-        const roleColor = args[1];
+        const roleColor = args[0];
+        const roleName = message.content.replace(prefix + commandName + ' ' + args[0], '');
 
         message.guild.roles.create({
             data: {
