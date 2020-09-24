8b61b12370175ad9e22dd76ff39703c20aca1b0c
Avi <67220821+avi-lime@users.noreply.github.com>
2020-09-24 17:24:40 +0530

    heist and giveaway changes
    added mention parsing in both and merged 2 heist files into one

--- autoresponses/heist.js
+++ autoresponses/heist.js
@@ -1,6 +1,47 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var host, amount;
+
+function getMetionedMember(mention) {
+    if (!mention) return;
+    if (mention.startsWith('<@') && mention.endsWith('>')) {
+        mention = mention.slice(2, -1);
+
+        if (mention.startsWith('!')) {
+            mention = mention.slice(1);
+        }
+
+        return client.guilds.cache.get('688102135363141652').members.cache.get(mention);
+    }
+}
+
+function getMentionedUser(mention) {
+    if (!mention) return;
+    if (mention.startsWith('<@') && mention.endsWith('>')) {
+        mention = mention.slice(2, -1);
+        if (mention.startsWith('!')) {
+            mention = mention.slice(1);
+        }
+        return client.users.cache.get(mention);
+    }
+}
+function getVal(val) {
+    multiplier = val.substr(-1).toLowerCase();
+    if (multiplier == "k")
+        return parseFloat(val) * 1000;
+    else if (multiplier == "m")
+        return parseFloat(val) * 1000000;
+    else if (!isNaN(val))
+        return parseFloat(val);
+    else if (multiplier == 'e') {
+        power = val.splice(0, -1);
+        Math.pow(parseFloat(val), power);
+        return;
+    }
+    else return message.channel.send(`Give me a valid number`);
+}
+
 client.on('ready', () => {
     console.log(`heist lock and unlock working`);
 });
@@ -13,7 +54,7 @@ client.on('message', message => {
         const begun = new Discord.MessageEmbed()
             .setTitle(`Heist Started!`)
             .setDescription(`The heist has begun\n ⋅ I've unlocked the channel, Good luck`)
-            .setColor('ORANGE');
+            .setColor(hostMember.displayHexColor);
         message.channel.send(begun);
     }
     if (message.content.match(/^Time is up to join/g) && message.author.id === '270904126974590976' && message.channel.id === '743202342714998857') {
@@ -21,20 +62,44 @@ client.on('message', message => {
         message.channel.updateOverwrite(message.channel.guild.roles.cache.get('749662555395326045'), { SEND_MESSAGES: null });
         const ended = new Discord.MessageEmbed()
             .setTitle(`Time up!`)
-            .setDescription(`Time's up to join the heist!\n - I've locked the channel!`)
+            .setDescription(`Time's up to join the heist!\n ⋅ I've locked the channel!`)
             .setColor('RED');
         message.channel.send(ended);
     }
     if (message.content.match(/^Amazing job everybody, we racked up a total of/g) && message.author.id === '270904126974590976' && message.channel.id === '743202342714998857') {
         const stay = new Discord.MessageEmbed()
             .setTitle(`Good Job everyone!`)
-            .setDescription(`<a:sevenmoney:750415278973648947> **Lots of giveaways**, **Daily heists** and **pepec** giveaway after heist, so stay for those!\n> **10 Million heist, Trophy, 2 Medals and 10 pepec giveaway at 1000 members**\n ⋅ check out <#737298442434379867> & <#688396273723637807>\n ⋅ get roles in <#688438603965268105>`)
+            .setDescription(`<a:sevenmoney:750415278973648947> Go thank ${host} in <#688102136243814471> for the heist, Daily 2 heists minimum and lots of giveaways, so stay for those!\n> **25 Million heist, Trophy, 2 Medals and 10 pepec giveaway at 1000 members**\n ⋅ check out <#737298442434379867> & <#688396273723637807>\n ⋅ get roles in <#688438603965268105>`)
             .setFooter(`Thanks for joining, have a lovely day~`)
-            .setColor('ORANGE');
+            .setColor(hostMember.displayHexColor);
         setTimeout(() => {
             message.channel.send(stay);
         }, 1500);
     }
+
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+    const heistchan = ['743202342714998857', '688109345187037402'];
+    if (!heistchan.includes(message.channel.id)) return;
+    const allowed = ['629768073414574110', '454307252392951819'];
+    if (!allowed.includes(message.author.id)) return;
+
+    if (commandName === 'heist') {
+        if (!args[0] || !args[1] || message.mentions.users.size === 0) return message.channel.send(`Correct usage is \`-heist <host> <amount>\``);
+        host = getMentionedUser(args[0]);
+        var hostMember = getMetionedMember(args[0]);
+        amount = getVal(args[1]);
+        var embed = new Discord.MessageEmbed()
+            .setTitle('<:sevenheist:750333392104718367>  :: HEIST TIME! × <:sevenheist:750333392104718367>')
+            .setColor(hostMember.displayHexColor)
+            .setThumbnail(host.displayAvatarURL({ dynamic: true }))
+            .addFields({ name: '<a:sevenrich:750415401694920727> Donator', value: host, inline: true }, { name: '<a:sevenmoney:750415278973648947> Amount', value: amount.toLocaleString(), inline: true }, { name: '\u200b', value: `> Keep 1000 ready, you'll only get 1 chance to say \`Join Heist\`!` })
+            .setFooter(`Good Luck!`);
+        message.delete().then
+        message.channel.send({ content: '<@&688428979153272860>', embed: embed });
+    }
+
 
 });
 
--- commands/gw.js
+++ commands/gw.js
@@ -5,23 +5,48 @@ module.exports = {
     name: 'gw',
     description: 'giveaway ping',
     execute(msg) {
+
+        function getMetionedMember(mention) {
+            if (!mention) return;
+            if (mention.startsWith('<@') && mention.endsWith('>')) {
+                mention = mention.slice(2, -1);
+
+                if (mention.startsWith('!')) {
+                    mention = mention.slice(1);
+                }
+
+                return client.guilds.cache.get('688102135363141652').members.cache.get(mention);
+            }
+        }
+
+        function getMentionedUser(mention) {
+            if (!mention) return;
+            if (mention.startsWith('<@') && mention.endsWith('>')) {
+                mention = mention.slice(2, -1);
+                if (mention.startsWith('!')) {
+                    mention = mention.slice(1);
+                }
+                return client.users.cache.get(mention);
+            }
+        }
+
         const args = msg.content.slice(prefix.length).trim().split(/ +/);
         const commandName = args.shift().toLowerCase();
         const gwchan = ['757238096508092496', '757238140120465458', '738643326772707408', '737298442434379867', '688108643966779420', '688109298852692055'];
         if (!msg.member.roles.cache.has('690566791407206431') || !gwchan.includes(msg.channel.id)) return;
-        const type = args[0];
         const bot = ['verse', 'myuu', 'pokemon'];
-        const host = msg.mentions.members.first();
 
-        if (!bot.includes(type) || !type) {
+        if (!bot.includes(args[0])) {
+            host = getMentionedUser(args[0]);
+            hostMember = getMetionedMember(args[0]);
             if (host) {
                 const gwmsg = msg.content.replace(prefix + commandName + ' ' + args[0], '');
                 if (gwmsg) {
                     const desc = new Discord.MessageEmbed()
                         .setTitle(`<a:sevengiveaway:750301687591338054> :: GIVEAWAY × <a:sevengiveaway:750301687591338054>`)
                         .setTimestamp()
-                        .setThumbnail(msg.mentions.users.first().displayAvatarURL({ dynamic: true }))
-                        .setColor(host.displayHexColor)
+                        .setThumbnail(host.displayAvatarURL({ dynamic: true }))
+                        .setColor(hostMember.displayHexColor)
                         .addFields({ name: '<a:sevenrich:750415401694920727> Donated by', value: host }, { name: '<:sevenmessage:750647888958324747> Message', value: gwmsg });
                     msg.delete().then
                     msg.channel.send({ content: '<@&688428862672994307>', embed: desc });
@@ -29,8 +54,8 @@ module.exports = {
                     const desc = new Discord.MessageEmbed()
                         .setTitle(`<a:sevengiveaway:750301687591338054> :: GIVEAWAY × <a:sevengiveaway:750301687591338054>`)
                         .setTimestamp()
-                        .setThumbnail(msg.mentions.users.first().displayAvatarURL({ dynamic: true }))
-                        .setColor(host.displayHexColor)
+                        .setThumbnail(host.displayAvatarURL({ dynamic: true }))
+                        .setColor(hostMember.displayHexColor)
                         .addFields({ name: '<a:sevenrich:750415401694920727> Donated by', value: host });
                     msg.delete().then
                     msg.channel.send({ content: '<@&688428862672994307>', embed: desc });
@@ -57,15 +82,17 @@ module.exports = {
                     msg.channel.send({ content: '<@&688428862672994307>', embed: desc });
                 }
             }
-        } else if (type === 'verse') {
+        } else if (args[0] === 'verse') {
+            host = getMentionedUser(args[1]);
+            hostMember = getMetionedMember(args[1]);
             if (host) {
                 const gwmsg = msg.content.replace(prefix + commandName + ' ' + args[0] + ' ' + args[1], '');
                 if (gwmsg) {
                     const desc = new Discord.MessageEmbed()
                         .setTitle(`<a:sevengiveaway:750301687591338054> :: GIVEAWAY × <a:sevengiveaway:750301687591338054>`)
                         .setTimestamp()
-                        .setThumbnail(msg.mentions.users.first().displayAvatarURL({ dynamic: true }))
-                        .setColor(host.displayHexColor)
+                        .setThumbnail(host.displayAvatarURL({ dynamic: true }))
+                        .setColor(hostMember.displayHexColor)
                         .addFields({ name: '<a:sevenrich:750415401694920727> Donated by', value: host }, { name: '<:sevenmessage:750647888958324747> Message', value: gwmsg });
                     msg.delete().then
                     msg.channel.send({ content: '<@&736943085115670568>', embed: desc });
@@ -73,8 +100,8 @@ module.exports = {
                     const desc = new Discord.MessageEmbed()
                         .setTitle(`<a:sevengiveaway:750301687591338054> :: GIVEAWAY × <a:sevengiveaway:750301687591338054>`)
                         .setTimestamp()
-                        .setThumbnail(msg.mentions.users.first().displayAvatarURL({ dynamic: true }))
-                        .setColor(host.displayHexColor)
+                        .setThumbnail(host.displayAvatarURL({ dynamic: true }))
+                        .setColor(hostMember.displayHexColor)
                         .addFields({ name: '<a:sevenrich:750415401694920727> Donated by', value: host });
                     msg.delete().then
                     msg.channel.send({ content: '<@&736943085115670568>', embed: desc });
@@ -103,13 +130,15 @@ module.exports = {
             }
         } else if (type === 'myuu') {
             if (host) {
+                host = getMentionedUser(args[1]);
+                hostMember = getMetionedMember(args[1]);
                 const gwmsg = msg.content.replace(prefix + commandName + ' ' + args[0] + ' ' + args[1], '');
                 if (gwmsg) {
                     const desc = new Discord.MessageEmbed()
                         .setTitle(`<a:sevengiveaway:750301687591338054> :: GIVEAWAY × <a:sevengiveaway:750301687591338054>`)
                         .setTimestamp()
-                        .setThumbnail(msg.mentions.users.first().displayAvatarURL({ dynamic: true }))
-                        .setColor(host.displayHexColor)
+                        .setThumbnail(host.displayAvatarURL({ dynamic: true }))
+                        .setColor(hostMember.displayHexColor)
                         .addFields({ name: '<a:sevenrich:750415401694920727> Donated by', value: host }, { name: '<:sevenmessage:750647888958324747> Message', value: gwmsg });
                     msg.delete().then
                     msg.channel.send({ content: '<@&736943394353578067>', embed: desc });
@@ -117,8 +146,8 @@ module.exports = {
                     const desc = new Discord.MessageEmbed()
                         .setTitle(`<a:sevengiveaway:750301687591338054> :: GIVEAWAY × <a:sevengiveaway:750301687591338054>`)
                         .setTimestamp()
-                        .setThumbnail(msg.mentions.users.first().displayAvatarURL({ dynamic: true }))
-                        .setColor(host.displayHexColor)
+                        .setThumbnail(host.displayAvatarURL({ dynamic: true }))
+                        .setColor(hostMember.displayHexColor)
                         .addFields({ name: '<a:sevenrich:750415401694920727> Donated by', value: host });
                     msg.delete().then
                     msg.channel.send({ content: '<@&736943394353578067>', embed: desc });
@@ -147,13 +176,15 @@ module.exports = {
             }
         } else if (type === 'pokemon') {
             if (host) {
+                host = getMentionedUser(args[1]);
+                hostMember = getMetionedMember(args[1]);
                 const gwmsg = msg.content.replace(prefix + commandName + ' ' + args[0] + ' ' + args[1], '');
                 if (gwmsg) {
                     const desc = new Discord.MessageEmbed()
                         .setTitle(`<a:sevengiveaway:750301687591338054> :: GIVEAWAY × <a:sevengiveaway:750301687591338054>`)
                         .setTimestamp()
-                        .setThumbnail(msg.mentions.users.first().displayAvatarURL({ dynamic: true }))
-                        .setColor(host.displayHexColor)
+                        .setThumbnail(host.displayAvatarURL({ dynamic: true }))
+                        .setColor(hostMember.displayHexColor)
                         .addFields({ name: '<a:sevenrich:750415401694920727> Donated by', value: host }, { name: '<:sevenmessage:750647888958324747> Message', value: gwmsg });
                     msg.delete().then
                     msg.channel.send({ content: '<@&737619474773049384>', embed: desc });
@@ -161,8 +192,8 @@ module.exports = {
                     const desc = new Discord.MessageEmbed()
                         .setTitle(`<a:sevengiveaway:750301687591338054> :: GIVEAWAY × <a:sevengiveaway:750301687591338054>`)
                         .setTimestamp()
-                        .setThumbnail(msg.mentions.users.first().displayAvatarURL({ dynamic: true }))
-                        .setColor(host.displayHexColor)
+                        .setThumbnail(host.displayAvatarURL({ dynamic: true }))
+                        .setColor(hostMember.displayHexColor)
                         .addFields({ name: '<a:sevenrich:750415401694920727> Donated by', value: host });
                     msg.delete().then
                     msg.channel.send({ content: '<@&737619474773049384>', embed: desc });
