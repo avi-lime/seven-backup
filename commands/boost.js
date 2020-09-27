2548d3eeddb165f0fcf6f99924abdd2e05f9591a
Avi <67220821+avi-lime@users.noreply.github.com>
2020-09-27 17:37:17 +0530

    Update boost.js

--- commands/boost.js
+++ commands/boost.js
@@ -9,7 +9,7 @@ module.exports = {
         const boost = new Discord.MessageEmbed()
             .setTitle(`__;; booster perks!!__`)
             .setDescription(`\u200b`)
-            .addFields({ name: `>          × single booster perks ×`, value: `\u200b\n\`🥯\` access to \`pls snipe\`\n\`🍓\` get pinged for **secret giveaways**\n\`🥯\` **add an emoji** to the server (sfw)\n\`🍓\` a custom **auto reaction** & the <@&737309369741606983> role\n\`🥯\` access to <#749644847501279302> \n\`🍓\` **attach files**/embed links \n\`🥯\` 100k mimu cash & **access to the \`.bubbletea\` command**\n` }, {
+            .addFields({ name: `>          × single booster perks ×`, value: `\u200b\n\`🥯\` access to \`pls snipe\`\n\`🍓\` get pinged for **secret giveaways**\n\`🥯\` **add an emoji** to the server (sfw)\n\`🍓\` a custom **auto reaction** & the <@&737309369741606983> role\n\`🥯\` access to <#749644847501279302> \n\`🍓\` **attach files**/embed links \n\`🥯\` 100k mimu cash & **access to the \`.bubbletea\` command**\n\u200b` }, {
                 name: `>          × double booster perks ×`, value: `\u200b\n\`🍓\` **all perks above**\n\`🥯\` add **another emoji** (sfw)\n\`🍓\` first **preference to their suggestions**, be included in **server discussions.**\n\`🥯\` get access to <#749637203381977148>  & vc⁴\n\`🍓\` a **private channel** ( you can **invite one friend** )\n\`🥯\` **own custom role with color** (not hoisted) & the <@&749638466618523778> & <@&735378963844694098>  role\n\`🍓\` another 100k mimu cash`
             }, { name: `\u200b`, value: `**__for more perks check out <#756925452748193843>__**` })
             .setColor(`#FF32F8`)
